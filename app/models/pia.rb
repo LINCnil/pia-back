2b6d92f41c0dbc666c7e3b769301c72b4400271e
Mathieu d'Alexis <mathieu@atnos.com>
2017-07-31 15:54:50 +0200

    Add duplicate for Pia model

--- app/models/pia.rb
+++ app/models/pia.rb
@@ -4,4 +4,27 @@ class Pia < ApplicationRecord
   has_many :evaluations, inverse_of: :pia
   has_many :measures, inverse_of: :pia
   validates :name, presence: true
+
+  def duplicate
+    duplicate_self
+    duplicate_answers
+    duplicate_comments
+    duplicate_evaluations
+    duplicate_measures
+  end
+
+  private
+
+  def duplicate_self
+    @clone = self.dup
+    @clone.save
+  end
+
+  %w(answers evaluations comments measures).each do |association|
+    define_method("duplicate_#{association}") do
+      send(association).each do |value|
+        @clone.send(association) << value.dup
+      end
+    end
+  end
 end
