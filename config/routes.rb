0acc0b004ce61429f6af06aedf39afbe039eacf6
Bruno Perles <bruno@atnos.com>
2017-09-18 11:42:22 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :pias
+  resources :pias do
+    member do
+      post '/duplicate', to: 'pias#duplicate'
+    end
+    collection do
+      post '/import', to: 'pias#import'
+    end
+  end
   resources :answers
   resources :comments
   resources :evaluations
   resources :measures
   resources :attachments
-
-  # Non CRUD routes
-  post '/pias/duplicate/:id', to: 'pias#duplicate'
-  post '/pias/import', to: 'pias#import'
 end
