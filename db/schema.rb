77b76b6da11aa8635c58c3971d2954403a627c01
Kosmas Schütz <kosmas.schuetz@gmx.com>
2018-03-10 09:20:07 +0100

    Updated to Rails 5.1.5 Replaced deprecated FactoryGirl through FactoryBot

--- db/schema.rb
+++ db/schema.rb
@@ -15,77 +15,77 @@ ActiveRecord::Schema.define(version: 20180223064141) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "answers", force: :cascade do |t|
-    t.string   "reference_to",              null: false
-    t.jsonb    "data",         default: {}
-    t.integer  "pia_id"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.index ["pia_id"], name: "index_answers_on_pia_id", using: :btree
+  create_table "answers", id: :serial, force: :cascade do |t|
+    t.string "reference_to", null: false
+    t.jsonb "data", default: {}
+    t.integer "pia_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pia_id"], name: "index_answers_on_pia_id"
   end
 
-  create_table "attachments", force: :cascade do |t|
-    t.string   "attached_file"
-    t.boolean  "pia_signed",    default: false
-    t.integer  "pia_id"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
-    t.text     "comment"
-    t.index ["pia_id"], name: "index_attachments_on_pia_id", using: :btree
+  create_table "attachments", id: :serial, force: :cascade do |t|
+    t.string "attached_file"
+    t.boolean "pia_signed", default: false
+    t.integer "pia_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "comment"
+    t.index ["pia_id"], name: "index_attachments_on_pia_id"
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.text     "description",  default: ""
-    t.string   "reference_to",                 null: false
-    t.boolean  "for_measure",  default: false
-    t.integer  "pia_id"
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.index ["pia_id"], name: "index_comments_on_pia_id", using: :btree
+  create_table "comments", id: :serial, force: :cascade do |t|
+    t.text "description", default: ""
+    t.string "reference_to", null: false
+    t.boolean "for_measure", default: false
+    t.integer "pia_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pia_id"], name: "index_comments_on_pia_id"
   end
 
-  create_table "evaluations", force: :cascade do |t|
-    t.integer  "status",                        default: 0
-    t.string   "reference_to",                               null: false
-    t.text     "action_plan_comment",           default: ""
-    t.text     "evaluation_comment",            default: ""
+  create_table "evaluations", id: :serial, force: :cascade do |t|
+    t.integer "status", default: 0
+    t.string "reference_to", null: false
+    t.text "action_plan_comment", default: ""
+    t.text "evaluation_comment", default: ""
     t.datetime "evaluation_date"
-    t.jsonb    "gauges",                        default: {}
+    t.jsonb "gauges", default: {}
     t.datetime "estimated_implementation_date"
-    t.string   "person_in_charge",              default: ""
-    t.integer  "pia_id"
-    t.datetime "created_at",                                 null: false
-    t.datetime "updated_at",                                 null: false
-    t.integer  "global_status",                 default: 0
-    t.index ["pia_id"], name: "index_evaluations_on_pia_id", using: :btree
+    t.string "person_in_charge", default: ""
+    t.integer "pia_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "global_status", default: 0
+    t.index ["pia_id"], name: "index_evaluations_on_pia_id"
   end
 
-  create_table "measures", force: :cascade do |t|
-    t.string   "title",       default: ""
-    t.text     "content",     default: ""
-    t.text     "placeholder", default: ""
-    t.integer  "pia_id"
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.index ["pia_id"], name: "index_measures_on_pia_id", using: :btree
+  create_table "measures", id: :serial, force: :cascade do |t|
+    t.string "title", default: ""
+    t.text "content", default: ""
+    t.text "placeholder", default: ""
+    t.integer "pia_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["pia_id"], name: "index_measures_on_pia_id"
   end
 
-  create_table "pias", force: :cascade do |t|
-    t.integer  "status",                   default: 0
-    t.string   "name",                                  null: false
-    t.string   "author_name",              default: ""
-    t.string   "evaluator_name",           default: ""
-    t.string   "validator_name",           default: ""
-    t.integer  "dpo_status",               default: 0
-    t.text     "dpo_opinion",              default: ""
-    t.text     "concerned_people_opinion", default: ""
-    t.integer  "concerned_people_status",  default: 0
-    t.text     "rejection_reason",         default: ""
-    t.text     "applied_adjustments",      default: ""
-    t.datetime "created_at",                            null: false
-    t.datetime "updated_at",                            null: false
-    t.string   "dpos_names",               default: ""
-    t.string   "people_names",             default: ""
+  create_table "pias", id: :serial, force: :cascade do |t|
+    t.integer "status", default: 0
+    t.string "name", null: false
+    t.string "author_name", default: ""
+    t.string "evaluator_name", default: ""
+    t.string "validator_name", default: ""
+    t.integer "dpo_status", default: 0
+    t.text "dpo_opinion", default: ""
+    t.text "concerned_people_opinion", default: ""
+    t.integer "concerned_people_status", default: 0
+    t.text "rejection_reason", default: ""
+    t.text "applied_adjustments", default: ""
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "dpos_names", default: ""
+    t.string "people_names", default: ""
   end
 
   add_foreign_key "answers", "pias"
--- test/factories/answers.rb
+++ test/factories/answers.rb
@@ -1,4 +1,4 @@
-FactoryGirl.define do
+FactoryBot.define do
   factory :answer do
     reference_to '1.1.2'
     pia
