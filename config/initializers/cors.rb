fa087e9c147d42870eba452ccbaae8edfcba9a7d
Kosmas Schütz <kosmas.schuetz@gmx.com>
2019-03-08 09:23:07 +0100

    Added cors stuff from community

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '*',
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    origins ENV['ALLOWED_CORS_ORIGINS'].split(',')
+    resource '*', headers: :any, methods: %i[get post put patch delete options head]
   end
 end
