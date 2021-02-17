aa9aa9230d5e96fc849ba6246b88081b903bafe8
Bruno Perles <bruno@atnos.com>
2021-02-17 16:54:09 +0100

    Add access_type array

--- lib/custom_token_response.rb
+++ lib/custom_token_response.rb
@@ -1,13 +1,15 @@
 module CustomTokenResponse
   def body
     user = User.find(@token.resource_owner_id)
+    access_type = []
+    access_type << "technical" if user.is_technical_admin
+    access_type << "functional" if user.is_functional_admin
+    access_type << "user" if user.is_user
     additional_data = {
       firstname: user.firstname,
       lastname: user.lastname,
       email: user.email,
-      technical: user.is_technical_admin,
-      functional: user.is_functional_admin,
-      user: user.is_user,
+      access_type: access_type,
       access_auth: [
         {
           id: nil,
