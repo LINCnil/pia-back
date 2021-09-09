a20325890c9b112c0e14555823d6b76864e9c87c
syl-p <sylvain@atnos.com>
2021-09-09 14:17:06 +0200

    feat: pass smtp config to mailer.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,16 +35,6 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
-  
-  config.action_mailer.smtp_settings = {
-    address: Rails.application.credentials.smtp_address,
-    port: Rails.application.credentials.smtp_port,
-    domain: Rails.application.credentials.smtp_domain,
-    user_name: Rails.application.credentials.smtp_user_name,
-    password: Rails.application.credentials.smtp_password,
-    authentication: Rails.application.credentials.smtp_authentication,
-    enable_starttls_auto: Rails.application.credentials.smtp_enable_starttls_auto
-  }
 
   config.action_mailer.perform_caching = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,16 +57,6 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  
-  config.action_mailer.smtp_settings = {
-    address: Rails.application.credentials.smtp_address,
-    port: Rails.application.credentials.smtp_port,
-    domain: Rails.application.credentials.smtp_domain,
-    user_name: Rails.application.credentials.smtp_user_name,
-    password: Rails.application.credentials.smtp_password,
-    authentication: Rails.application.credentials.smtp_authentication,
-    enable_starttls_auto: Rails.application.credentials.smtp_enable_starttls_auto
-  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
