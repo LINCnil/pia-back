b6ac48b2c1000b13d79df45d93cc6250f1c42892
BEYRAND Kévin <kevin@atnos.com>
2021-09-10 15:12:28 +0200

    add all languages on pia-back

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def uuid_created
     @user = params[:user]
 
-    mail(to: @user.email, subject: I18n.t('mail.uuid_subject'))
+    mail(to: @user.email, subject: I18n.t('email_new_account_uuid.subject'))
   end
 end
