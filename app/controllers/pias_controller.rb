a6cdda08acc62d461115907ae0cb8ae25c8733b1
syl-p <sylvain@atnos.com>
2021-09-10 15:33:46 +0200

    refactor: add string refactoring

--- app/controllers/pias_controller.rb
+++ app/controllers/pias_controller.rb
@@ -33,19 +33,19 @@ class PiasController < ApplicationController
 
     if check_user_id(pia_parameters[:author_name]).is_a?(User)
       user = check_user_id(pia_parameters[:author_name])
-      @pia.author_name = user.firstname + " " + user.lastname
+      @pia.author_name = "#{user.firstname} #{user.lastname}"
       @pia.user_pias << UserPia.new(user_id: user.id, role: 1)
     end
 
     if check_user_id(pia_parameters[:evaluator_name]).is_a?(User)
       user = check_user_id(pia_parameters[:evaluator_name])
-      @pia.evaluator_name = user.firstname + " " + user.lastname
+      @pia.evaluator_name = "#{user.firstname} #{user.lastname}"
       @pia.user_pias << UserPia.new(user_id: user.id, role: 2)
     end
 
     if check_user_id(pia_parameters[:validator_name]).is_a?(User)
       user = check_user_id(pia_parameters[:validator_name])
-      @pia.validator_name = user.firstname + " " + user.lastname
+      @pia.validator_name = "#{user.firstname} #{user.lastname}"
       @pia.user_pias << UserPia.new(user_id: user.id, role: 3)
     end
 
@@ -74,7 +74,7 @@ class PiasController < ApplicationController
         end
 
         # update author_name value
-        @pia.author_name = user.firstname + " " + user.lastname
+        @pia.author_name = "#{user.firstname} #{user.lastname}"
       end
   
       if check_user_id(pia_parameters[:evaluator_name]).is_a?(User)
@@ -87,7 +87,7 @@ class PiasController < ApplicationController
           relation.save
         end
 
-        @pia.evaluator_name = user.firstname + " " + user.lastname
+        @pia.evaluator_name = "#{user.firstname} #{user.lastname}"
       end
   
       if check_user_id(pia_parameters[:validator_name]).is_a?(User)
@@ -100,7 +100,7 @@ class PiasController < ApplicationController
           relation.save
         end
 
-        @pia.validator_name ="#{user.firstname} #{user.lastname}"
+        @pia.validator_name = "#{user.firstname} #{user.lastname}"
       end
 
       @pia.save
