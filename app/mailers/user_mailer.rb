1449c04358b31cb7063d51280e784de52a0ba1b8
syl-p <sylvain@atnos.com>
2021-09-21 16:11:19 +0200

    feat: update userMailer with i18n keys

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,9 @@ class UserMailer < ApplicationMailer
 
     mail(to: @user.email, subject: I18n.t('email_new_account_uuid.subject'))
   end
+  def uuid_updated
+    @user = params[:user]
+
+    mail(to: @user.email, subject: I18n.t('email_recover_account_uuid.subject'))
+  end
 end
