f5261bd29e8e30b1316215ac8c6e52cbbf40280d
syl-p <sylvain@atnos.com>
2021-10-29 11:32:55 +0200

    feat: add password validation with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, :rememberable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :validatable, :lockable
+
   validates :uuid, presence: true
+  validates :password, format: {with: /\A(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}\z/}
   
   before_validation :generate_uuid
   # after_update :generate_uuid, if: self.changed.include?("password")
@@ -19,5 +21,12 @@ class User < ApplicationRecord
 
   def generate_uuid
     self.uuid = SecureRandom.uuid
+  end
+
+  private
+  
+  def password_complexity
+    return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
+    errors.add :password, 'Complexity requirement not met. Length should be 8-70 characters and include: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
   end
 end
