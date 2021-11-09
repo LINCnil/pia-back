f43942ffbdd61874c622b93d54d637151a7fb1d0
syl-p <sylvain@atnos.com>
2021-11-09 11:41:55 +0100

    feat: authorize_pia only for auth mode

--- app/controllers/pias_controller.rb
+++ app/controllers/pias_controller.rb
@@ -1,6 +1,6 @@
 class PiasController < ApplicationController
   before_action :set_pia, only: %i[show update destroy duplicate]
-  before_action :authorize_pia
+  before_action :authorize_pia if ENV['ENABLE_AUTHENTICATION'].present?
 
   # GET /pias
   def index
