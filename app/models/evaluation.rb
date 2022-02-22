30eab63af75bd633f2f30adaaa79a0787aac2043
sylv-p <sylvain@atnos.com>
2022-02-22 15:12:07 +0100

    refactor: move condition on top

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -3,21 +3,19 @@ class Evaluation < ApplicationRecord
   validates :reference_to, presence: true
   attr_accessor :evaluation_infos
 
-  after_create :email_for_evaluation!
+  after_create :email_for_evaluation! if ENV['ENABLE_AUTHENTICATION'].present?
 
   def email_for_evaluation!
-    if ENV['ENABLE_AUTHENTICATION'].present?
-      infos = self.evaluation_infos
-      evaluation_mode = infos["evaluation_mode"]
-      questions = infos["questions"]
-      author = self.pia.user_pias.find_by({role: "author"}).user
-      evaluator = self.pia.user_pias.find_by({role: "evaluator"}).user
+    infos = self.evaluation_infos
+    evaluation_mode = infos["evaluation_mode"]
+    questions = infos["questions"]
+    author = self.pia.user_pias.find_by({role: "author"}).user
+    evaluator = self.pia.user_pias.find_by({role: "evaluator"}).user
 
-      if evaluation_mode === 'item' || 
-        (evaluation_mode === 'question' && questions[0]["id"] == self.reference_to.split(".").last.to_i)
-        UserMailer.with(evaluator: evaluator, pia: self.pia).section_ready_for_evaluation.deliver_now
-      end
-      byebug
+    if evaluation_mode === 'item' || 
+      (evaluation_mode === 'question' && questions[0]["id"] == self.reference_to.split(".").last.to_i)
+      UserMailer.with(evaluator: evaluator, pia: self.pia).section_ready_for_evaluation.deliver_now
     end
+    byebug
   end
 end
