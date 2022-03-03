bd91f72ba19f7ff098cd44a7b71ecc64464f2a6f
sylv-p <sylvain@atnos.com>
2022-03-03 16:47:40 +0100

    feat: prepare condition for sending email

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -4,6 +4,7 @@ class Evaluation < ApplicationRecord
   attr_accessor :evaluation_infos
 
   after_create :email_for_evaluation! if ENV['ENABLE_AUTHENTICATION'].present?
+  after_update :email_for_validation! if ENV['ENABLE_AUTHENTICATION'].present?
 
   def email_for_evaluation!
     infos = self.evaluation_infos
@@ -15,6 +16,20 @@ class Evaluation < ApplicationRecord
     if evaluation_mode === 'item' || 
       (evaluation_mode === 'question' && questions[0]["id"] == self.reference_to.split(".").last.to_i)
       UserMailer.with(evaluator: evaluator, pia: self.pia).section_ready_for_evaluation.deliver_now
+    end
+  end
+
+  def email_for_validation!
+    infos = self.evaluation_infos
+    if infos.present?
+      evaluation_mode = infos["evaluation_mode"]
+      questions = infos["questions"]
+      if evaluation_mode === 'item' || 
+        (evaluation_mode === 'question' && questions[0]["id"] == self.reference_to.split(".").last.to_i)
+        if self.global_status == 2
+          # todo: send email to validator 
+        end
+      end
     end
   end
 end
