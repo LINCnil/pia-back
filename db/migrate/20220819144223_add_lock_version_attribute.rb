02641de5316c7bfb0899269ebffeeb1a50b713cb
syl-p <sylvain@atnos.com>
2022-09-05 11:48:26 +0200

    feat: update knowledge to check conflict + update db migration

--- db/migrate/20220819144223_add_lock_version_attribute.rb
+++ db/migrate/20220819144223_add_lock_version_attribute.rb
@@ -3,5 +3,7 @@ class AddLockVersionAttribute < ActiveRecord::Migration[7.0]
     add_column :pias, :lock_version, :integer, default: 0, null: false
     add_column :answers, :lock_version, :integer, default: 0, null: false
     add_column :measures, :lock_version, :integer, default: 0, null: false
+    add_column :knowledge_bases, :lock_version, :integer, default: 0, null: false
+    add_column :knowledges, :lock_version, :integer, default: 0, null: false
   end
 end
