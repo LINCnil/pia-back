421a6446fcdb04896dbaff109ce85813447c5992
syl-p <sylvain@atnos.com>
2023-01-30 12:04:55 +0100

    feat: prepare test for user + authentification is enable

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create(:user)
+  end
+
+  test "user is locked by default" do
+    expect(@user.is_locked).to eq(true)
+  end
 end
--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,4 +1,16 @@
 FactoryBot.define do
   factory :user do
+    transient do
+      is_user { true }
+      is_functional_admin { false }
+      is_technical_admin { false }
+    end
+
+    email { "user#{role.present? ? '+' +  role : nil}@test.com" }
+    firstname {'user'}
+    lastname {role ? role : 'nothing'}
+    is_user {is_user}
+    is_functional_admin {is_functional_admin}
+    is_technical_admin {is_technical_admin}
   end
 end
