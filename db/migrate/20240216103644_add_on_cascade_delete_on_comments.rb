fdfeaa8815918a1e51c79ad2106cdd14de56ac53
syl-p <sylvain@atnos.com>
2024-02-16 14:08:54 +0100

    feat: use nullify instead on cascade delete

--- db/migrate/20240216103644_add_on_cascade_delete_on_comments.rb
+++ db/migrate/20240216103644_add_on_cascade_delete_on_comments.rb
@@ -1,6 +1,6 @@
 class AddOnCascadeDeleteOnComments < ActiveRecord::Migration[7.0]
   def change
     remove_foreign_key :comments, :users
-    add_foreign_key :comments, :users, on_delete: :cascade
+    add_foreign_key :comments, :users, on_delete: :nullify
   end
 end
