3b2ece207abaf23b65dcbc99e1f180279ddb2eb4
syl-p <sylvain@atnos.com>
2024-02-23 10:18:01 +0100

    fix: fix test for pia and user_pias

--- test/controllers/pias_controller_test.rb
+++ test/controllers/pias_controller_test.rb
@@ -5,12 +5,13 @@ class PiasControllerTest < ActionDispatch::IntegrationTest
   include AuthorizationHelper
   setup do
     @pia = FactoryBot.create(:pia)
+    @doorkeeper_token = doorkeeper_token
     if ENV['ENABLE_AUTHENTICATION'].present?
       @auth = FactoryBot.create(:access_token)
       @admin = FactoryBot.create(:user_admin, identifier: "admin")
       @auth_tokens = auth_tokens_for_user(@admin, @auth)
 
-      @@doorkeeper_token = @auth_tokens['access_token']
+      @doorkeeper_token = @auth_tokens['access_token']
       # create users
       @user_functional = FactoryBot.create(:user, identifier: "functional")
       @user_user = FactoryBot.create(:user, identifier: "user")
@@ -41,11 +42,15 @@ class PiasControllerTest < ActionDispatch::IntegrationTest
     pia_params = {}
     if ENV['ENABLE_AUTHENTICATION'].present?
       # assign users
-      pia_params["authors"] = [@user_functional.id, @user_user.id]
-      pia_params["validators"] = [@user_user.id]
+      pia_params[:authors] = [@user_functional.id, @user_user.id].join(",")
+      pia_params[:validators] = [@user_user.id].join(",")
     end
+
     patch pia_url(@pia), params: { pia: pia_params }, headers: { 'Authorization' => "Bearer #{@doorkeeper_token}" }, as: :json
+    pia_response = JSON.parse(response.body)
+
     assert_response 200
+    assert_equal pia_response["user_pias"].count, 3
   end
 
   test 'should destroy pia' do
