2463325a258957ca4462621f727e9e781414a9e6
Kevin Beyrand <kevin@atnos.com>
2026-02-03 09:31:17 +0100

    fix: answers required fields management on the backend

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -33,7 +33,7 @@ class AnswersController < ApplicationController
 
   # POST /answers
   def create
-    @answer = Answer.new(answer_params)
+    @answer = Answer.new(processed_answer_params)
 
     if @answer.save
       render json: serialize(@answer.reload), status: :created
@@ -44,7 +44,7 @@ class AnswersController < ApplicationController
 
   # PATCH/PUT /answers/1
   def update
-    if @answer.update(answer_params)
+    if @answer.update(processed_answer_params)
       render json: serialize(@answer.reload)
     else
       render json: @answer.errors, status: :unprocessable_entity
@@ -71,9 +71,15 @@ class AnswersController < ApplicationController
 
   # Only allow a trusted parameter "white list" through.
   def answer_params
-    params.fetch(:answer, {}).permit(
-      :reference_to, :lock_version,
-      data: [:text, :gauge, { list: [] }]
-    ).merge(params.permit(:pia_id))
+    params.permit(:pia_id, :id, answer: [:reference_to, :lock_version, :data])
+    params.require(:answer).permit(
+      :reference_to, :lock_version, :data
+    ).merge(pia_id: params[:pia_id])
+  end
+
+  def processed_answer_params
+    answer_parameters = answer_params
+    answer_parameters[:data] = JSON.parse(answer_parameters[:data]) if answer_parameters[:data].is_a?(String)
+    answer_parameters
   end
 end
