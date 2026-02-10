9f81c679fb785e1d3d17c9b9842fd638afecfc5e
Kevin Beyrand <kevin@atnos.com>
2026-02-10 14:21:42 +0100

    feat: add models tests

--- test/models/measure_test.rb
+++ test/models/measure_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class MeasureTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include FactoryBot::Syntax::Methods
+
+  test "should be valid with valid attributes" do
+    measure = build(:measure)
+    assert measure.valid?
+  end
+
+  test "should belong to pia" do
+    measure = create(:measure)
+    assert_instance_of Pia, measure.pia
+  end
+
+  test "should sanitize title after initialize" do
+    measure = Measure.new(title: '<script>alert("xss")</script>Hello')
+    assert_not_includes measure.title, '<script>'
+  end
+
+  test "should sanitize content after initialize" do
+    measure = Measure.new(content: '<script>alert("xss")</script>Hello')
+    assert_not_includes measure.content, '<script>'
+  end
+
+  test "should sanitize placeholder after initialize" do
+    measure = Measure.new(placeholder: '<script>alert("xss")</script>Hello')
+    assert_not_includes measure.placeholder, '<script>'
+  end
 end
