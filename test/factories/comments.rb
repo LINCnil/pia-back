fb2712ad2b58b76ac7599448d970856013c4dd6d
Kevin Beyrand <kevin@atnos.com>
2026-02-10 14:57:47 +0100

    feat: update factories

--- test/factories/comments.rb
+++ test/factories/comments.rb
@@ -1,6 +1,17 @@
 FactoryBot.define do
   factory :comment do
-    reference_to { '1.1.2' }
+    sequence(:reference_to) { |n| "1.#{n}.1" }
+    description { 'Sample comment description' }
+    for_measure { false }
     pia
+    user { nil }
+
+    trait :for_measure do
+      for_measure { true }
+    end
+
+    trait :with_user do
+      user
+    end
   end
 end
