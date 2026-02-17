d17164c21d9652a61a9601ef31c0dae1910eff8b
Kevin Beyrand <kevin@atnos.com>
2026-02-17 16:13:06 +0100

    FEAT: Add models tests (#357)
    * feat: add models tests
    
    * CHORE: Rubocop (lint)

--- test/models/knowledge_test.rb
+++ test/models/knowledge_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class KnowledgeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  include FactoryBot::Syntax::Methods
+
+  test "should be valid with valid attributes" do
+    knowledge = build(:knowledge)
+    assert knowledge.valid?
+  end
+
+  test "should belong to knowledge_base" do
+    knowledge = create(:knowledge)
+    assert_instance_of KnowledgeBase, knowledge.knowledge_base
+  end
+
+  test "should require name" do
+    knowledge = build(:knowledge, name: nil)
+    assert_not knowledge.valid?
+    assert_includes knowledge.errors[:name], "can't be blank"
+  end
+
+  test "should require knowledge_base" do
+    knowledge = build(:knowledge, knowledge_base: nil)
+    assert_not knowledge.valid?
+    assert_includes knowledge.errors[:knowledge_base], "can't be blank"
+  end
+
+  test "should sanitize name after initialize" do
+    knowledge = Knowledge.new(name: '<script>alert("xss")</script>Hello')
+    assert_not_includes knowledge.name, '<script>'
+  end
 end
