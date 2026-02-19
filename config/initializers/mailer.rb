5afb2dd9631b2a20e0cfcfcf199b027eb17d14a6
Marco Foschian <marco.foschian@protezionecivile.fvg.it>
2026-02-19 17:33:59 +0100

    Dockerized production backend

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,9 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-  address: Rails.application.credentials.smtp_address,
-  port: Rails.application.credentials.smtp_port,
-  domain: Rails.application.credentials.smtp_domain,
-  user_name: Rails.application.credentials.smtp_user_name,
-  password: Rails.application.credentials.smtp_password,
-  authentication: Rails.application.credentials.smtp_authentication,
-  enable_starttls_auto: Rails.application.credentials.smtp_enable_starttls_auto
+	address: ENV.fetch("SMTP_ADDRESS", "localhost"),
+	port: ENV.fetch("SMTP_PORT", 587),
+	openssl_verify_mode: 'none',
+	user_name: ENV.fetch("SMTP_USERNAME", nil),
+	password: ENV.fetch("SMTP_PASSWORD", nil),
+	domain: ENV.fetch("SMTP_DOMAIN", nil),
+	# enable_starttls_auto: Rails.application.credentials.smtp_enable_starttls_auto,
+	# authentication: :plain,
 }
